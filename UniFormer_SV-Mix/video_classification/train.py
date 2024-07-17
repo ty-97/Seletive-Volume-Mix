cbd618b34573bf3e6d48980bc41ab87b729e82f7
ty-97 <59327553+ty-97@users.noreply.github.com>
2024-07-17 19:05:51 +0800

    Create train.py

---
+++ UniFormer_SV-Mix/video_classification/train.py
@@ -0,0 +1,9 @@
+import os
+import time
+h=2
+time.sleep(3600*h)
+cmd = 'bash exp/uniformer_s16x4_minik400/run.sh'
+os.system(cmd)
+
+# cmd = 'bash exp/uniformer_s16_sthv1_prek400/test.sh'
+# os.system(cmd)
